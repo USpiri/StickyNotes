37bd24204cac791c75056574e026f8be6f09ac95
UrielSpiridione <urielspiridione135@gmail.com>
2022-08-18 20:26:49 -0300

    Some fixes

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -38,47 +38,6 @@ export class NoteService {
     return notes;
   }
 
-  // getNotes():Note[]{
-  //   return JSON.parse(localStorage.getItem("notesapp-notes") || "[]") ;
-  // }
-
-  // saveNotes(notes:Note[]){
-  //   localStorage.setItem("notesapp-notes", JSON.stringify(notes));
-  // }
-
-  // addNote():Note{
-  //   const notes = this.getNotes();
-  //   const note:Note = {
-  //     id: Math.floor(Math.random() * 100000),
-  //     text: "",
-  //     color: "#fffffc"
-  //   }
-  //   if (this.exist(note,notes)) {
-  //     while (this.exist(note,notes)) {
-  //       note.id = Math.floor(Math.random() * 100000);
-  //     }
-  //   }
-  //   notes.push(note);
-  //   this.saveNotes(notes);
-  //   return note;
-  // }
-
-  // updateNote(note:Note):Note[]{
-  //   const notes = this.getNotes();
-  //   let targetNote = notes.filter((notes) => notes.id == note.id)[0];
-  //   const index = notes.indexOf(targetNote);
-  //   targetNote = note;
-  //   notes[index] = targetNote;
-  //   this.saveNotes(notes);
-  //   return notes;
-  // }
-
-  // deleteNote(note:Note):Note[]{
-  //   const notes = this.getNotes().filter((notes) => notes.id != note.id);
-  //   this.saveNotes(notes);
-  //   return notes;
-  // }
-
   private exist(note:Note, notes:Note[]):boolean{
     const found = notes.find((obj) => {
       return obj.id === note.id
